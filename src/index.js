71a1757c6058ff791609d817bfa424495b8bcce2
Agustin Perez <agustinnicolas806@gmail.com>
2022-05-17 18:40:12 -0300

    small changes

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ const cors = require('cors');
 const app = express();
 const PORT = 3005;
 
-const whiteList = ['http://localhost:3000', 'http://localhost:3005']
+const whiteList = ['http://localhost:3000', 'http://localhost:3005', 'https://git.heroku.com/intense-spire-90736.git'];
 
 app.use( cors({ origin: whiteList }) );
 app.use( morgan('dev') );
 app.use( express.json() );
+
+app.get( '/', () => console.log('Working...'));
 
 app.use('/api/notes', require('./routes/notes.router'));
 
